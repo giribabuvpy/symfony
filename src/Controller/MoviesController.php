389f0930dcfd6210717194cfa8ab8c909a9578c0
Hemanathagiribabu <giribabuvpy@gmail.com>
2024-03-18 22:46:22 +0530

    first commit

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MoviesController extends AbstractController
+{
+    #[Route('/movies', name: 'app_movies')]
+    public function index(): Response
+    {
+        return $this->render('movies/index.html.twig', [
+            'controller_name' => 'MoviesController',
+        ]);
+    }
+
+    #[Route('/show', name: 'app_show')]
+    public function showOne(): Response
+    {
+        return new Response("Hi");
+    }
+}
